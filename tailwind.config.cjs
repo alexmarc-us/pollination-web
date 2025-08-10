e9505c2b0587a33036bbc1a537f94e3e38196290
Alex Marcus <mr@alexmarc.us>
2025-08-10 09:55:56 -0700

    Removes dark theme and unused pages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'none',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     colors: {
